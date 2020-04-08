974e6be048291fe082243f7154c14829347aac29
Jefferson Yuri Cunha Burk <yuriburk@gmail.com>
2020-04-08 01:11:21 -0300

    Implementado desafios

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,68 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repository = repositories.find(r => r.id === id);
+
+  if (!repository) {
+    return response.status(400).json({error: 'Invalid repository Id.'});
+  }
+
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const { id } = req.params;
+
+  const repositoryIndex = repositories.findIndex(r => r.id === id);
+
+  if (repositoryIndex < 0) {
+    return res.status(400).json({error: 'Invalid repository Id.'});
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return res.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find(r => r.id === id);
+  
+  if (!repository) {
+    return response.status(400).json({error: 'Invalid repository Id.'});
+  }
+
+  repository.likes += 1;
+
+  return response.status(200).json(repository);
 });
 
 module.exports = app;
